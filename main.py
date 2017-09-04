aa92bb5b3c44345fea9b22ecc38d8343bd28a361
Krishan Bhasin <krishan.bhasin42+github@gmail.com>
2017-09-04 18:24:15 +0100

    initial-commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import googlemaps
+from datetime import datetime, timedelta
+import math
+import requests
+import json
+import iso8601
+
+from googlemaps import directions as directions_client
+
+with open("../config/configuration.json") as json_data_file:
+    configuration = json.load(json_data_file)
+    GMAPS_API_KEY = configuration["credentials"]["googleMapsApiKey"]
+    options = configuration["options"]
+    HOME_POSTCODE = options["homePostcode"]
+    BUS_STOP_POSTCODE = options["destinationPostcode"]
+    BUS_STOP_CODE = options["busStopCode"]
+    BUS_NUMBER = options["busNumber"]
+    TRAVEL_MODE = options["travelModeToBus"]
+
+NCTX_BASE_URL = "https://api.nctx.co.uk/api/v1/"
+
+
+def get_walking_time(travel_from, travel_to, travel_mode):
+    gmaps = googlemaps.Client(GMAPS_API_KEY)
+    directions = directions_client.directions(
+        origin=travel_from,
+        destination=travel_to,
+        client=gmaps,
+        mode=travel_mode
+    )
+    return math.ceil(directions[0]["legs"][0]["distance"]["value"] / 60)
+
+
+def get_next_bus_time(bus_number, bus_stop_code):
+    r = requests.get(NCTX_BASE_URL + "departures/" + bus_stop_code + "/realtime")
+
+    bus_time_json = json.loads(r.content.decode("utf-8"))
+
+    for entry in bus_time_json:
+        if entry["busService"] == bus_number:
+            return iso8601.parse_date(entry["time"]).replace(tzinfo=None)
+
+
+def main():
+    walking_time_minutes = get_walking_time(HOME_POSTCODE, BUS_STOP_POSTCODE, TRAVEL_MODE)
+    print("Time to bus stop: " + str(walking_time_minutes))
+    next_bus_time = get_next_bus_time(BUS_NUMBER, BUS_STOP_CODE)
+    print("Next bus time is: " + str(next_bus_time))
+    time_to_leave = next_bus_time - timedelta(minutes=walking_time_minutes)
+    print("The time to leave is: " + str(time_to_leave))
+
+    time_until_time_to_leave = time_to_leave - datetime.now()
+
+    print("You need to leave in " + str(time_until_time_to_leave))
+
+
+if __name__ == "__main__":
+    main()
